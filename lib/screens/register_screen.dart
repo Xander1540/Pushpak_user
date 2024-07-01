8007485bed9bbc01a95e1c202ba98ae3e1e596ca
Xander1540 <adityaxxx2003@gmail.com>
2024-07-01 14:03:06 +0530

    second commit

---
+++ lib/screens/register_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+
+  final nameTextEditingController = TextEditingController();
+  final emailTextEditingController = TextEditingController();
+  final phoneTextEditingController = TextEditingController();
+  final addressTextEditingController = TextEditingController();
+  final passwordTextEditingController = TextEditingController();
+  final confirmTextEditingController = TextEditingController();
+
+  bool _passwordVisible = false;
+
+  final _formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    
+    bool darkTheme = MediaQuery.of(context).platformBrightness == Brightness.dark;
+    
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        body: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            Column(
+              children: [
+                Image.asset(darkTheme ? 'images/cityNight.jpg' : 'images/cityDay.jpg'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
