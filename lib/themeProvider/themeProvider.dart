c7bc175da4056d1f1637286f6785c393225e5b1f
ADITYA NARAYAN VERMA <adityaxxx2003@gmail.com>
2024-07-07 21:41:01 +0530

    Update themeProvider.dart

--- lib/themeProvider/themeProvider.dart
+++ lib/themeProvider/themeProvider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyThemes{
   static final darkTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.grey.shade900,
+    scaffoldBackgroundColor: Colors.grey.shade800,
     colorScheme: ColorScheme.dark(),
   );
 
